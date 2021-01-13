425448c33b060e1e7a914cce3ba7922aae33e562
liupan <1206284818@qq.com>
2021-01-13 22:11:54 +0800

    NC72：二叉树镜像，构建二叉树

--- 0-nowcoder/NC0072/main.cpp
+++ 0-nowcoder/NC0072/main.cpp
@@ -4,6 +4,7 @@
 
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
 struct TreeNode {
@@ -29,25 +30,57 @@ class Solution {
 public:
     void Mirror(TreeNode *pRoot) {
         if (pRoot) {
-            TreeNode* tmp = pRoot->left;
+            TreeNode *tmp = pRoot->left;
             pRoot->left = pRoot->right;
             pRoot->right = tmp;
             Mirror(pRoot->left);
             Mirror(pRoot->right);
         }
     }
-    void preOrder(TreeNode* root, vector<int> &ret) {
+
+    /**
+     * 先序输出
+     * @param root
+     */
+    void preOrder(TreeNode *root) {
         if (root) {
-            ret.push_back(root->val);
-            preOrder(root->left, ret);
-            preOrder(root->right, ret);
+            cout << root->val << " ";
+            preOrder(root->left);
+            preOrder(root->right);
         }
+    }
+
+    /**
+     * 构建二叉树
+     * @param a
+     * @param i
+     * @return
+     */
+    TreeNode *createTree(vector<int> &a, int &i) {
+        TreeNode *root = nullptr;
+        if (i < a.size()) {
+            root = new TreeNode(a[i]);
+            root->left = createTree(a, ++i);
+            root->right = createTree(a, ++i);
+        }
+        return root;
     }
 };
 
 int main() {
     Solution s;
-    vector<int> ret;
-    
-
+//    TreeNode* pRoot = new TreeNode(8);
+//    pRoot->left = new TreeNode(6);
+//    pRoot->right = new TreeNode(10);
+//    pRoot->left->left = new TreeNode(5);
+//    pRoot->left->right = new TreeNode(7);
+//    pRoot->right->left = new TreeNode(9);
+//    pRoot->right->right = new TreeNode(11);
+    vector<int> a = {8, 6, 10, 5, 7, 9, 11};
+    int i = 0;
+    TreeNode *pRoot = s.createTree(a, i);
+    s.preOrder(pRoot);
+    cout << endl;
+    s.Mirror(pRoot);
+    s.preOrder(pRoot);
 }
