9c472db2e315336e00163654e05a8691a35e9ace
silianpan <liu.pan@silianpan.cn>
2021-02-08 22:41:25 +0800

    NC50 链表中的节点每k个一组翻转

--- 0-nowcoder/NC0050/main.cpp
+++ 0-nowcoder/NC0050/main.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
 struct ListNode {
@@ -27,40 +28,85 @@ public:
      */
     ListNode* reverseKGroup(ListNode* head, int k) {
         // write code here
-        // 求链表长度
-        int len = 0;
-        ListNode *start = head;
-        while (start) {
-            ++len;
-            start = start->next;
-        }
-        // 特列：长度小于k
-        if (len < k) return head;
+        int kk = k;
+        ListNode *new_head = head;
+        ListNode *cur = head;
+        ListNode *nex = nullptr;
+        ListNode *end = nullptr;
 
-        start = head;
-        // 遍历次数
-        int loop = len / k;
+        // 遍历链表
+        while (cur) {
+            // 找到第k次尾
+            if (kk == 1) {
+                // 保存每组末尾下一位
+                nex = cur->next;
+                // 截断每组
+                cur->next = nullptr;
+                // 反转当前组链表，并返回新的列表第一位
+                ListNode *temp = reverseKGroup(head);
+                // 如果是第一次循环，返回新的首地址
+                if (new_head == head) new_head = temp;
+                // 如果结尾不为空，下一位即为当前
+
+                // 拼接每组新开始和新结尾
+                if (end) end->next = cur;
+
+                // 因为反转了，所以设置结尾为开头
+                end = head;
+
+                // 设置开头为下一组开头
+                head = nex;
+                // 设置当前为下一组开头
+                cur = nex;
+                kk = k;
+                continue;
+            }
+            --kk;
+            cur = cur->next;
+        }
+        // 如果节点数不是k的倍数，将剩余的串联起来
+        if (end) end->next = head;
+        return new_head;
+    }
+
+    /**
+     * 反转链表
+     * @param head
+     * @return
+     */
+    ListNode* reverseKGroup(ListNode* head) {
         ListNode *pre = nullptr;
         ListNode *nex = nullptr;
-        ListNode *cur = start;
-        ListNode *end = nullptr;
-        for (int i = 0; i < loop; ++i) {
-            for (int j = 0; j < k; ++j) {
-                // 反转k长度列表
-                end = cur;
-                nex = cur->next;
-                cur->next = pre;
-                pre = cur;
-                cur = nex;
-            }
-            cur->next = end;
+        ListNode *cur = head;
+        while (cur) {
+            nex = cur->next;
+            cur->next = pre;
             pre = cur;
             cur = nex;
-            nex = cur->next;
         }
+        return pre;
     }
 };
 
 int main() {
-
+    vector<int> num = {1, 2, 3, 4, 5};
+    ListNode *head;
+    ListNode *start;
+    for (vector<int>::iterator it = num.begin(); it != num.end(); ++it) {
+        if (it == num.begin()) {
+            start = head = new ListNode;
+            start->val = *it;
+            head->val = *it;
+        } else {
+            start->next = new ListNode;
+            start->next->val = *it;
+            start = start->next;
+        }
+    }
+    Solution s;
+    ListNode *ret = s.reverseKGroup(head, 2);
+    while (ret) {
+        cout << ret->val << " ";
+        ret = ret->next;
+    }
 }
