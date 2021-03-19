147299461ed65d00c2192a3cb08b39c7c1d7e6f9
liupan <1206284818@qq.com>
2021-03-20 06:38:25 +0800

    算法29

--- 0029-divide-two-integers/main.cpp
+++ 0029-divide-two-integers/main.cpp
@@ -8,15 +8,44 @@
 //
 
 #include <iostream>
+
 using namespace std;
 
 class Solution {
 public:
-    int divide(int dividend, int divisor) {
+//    int sameSign(int a, int b) {
+//        return a ^ b >= 0;
+//    }
+//    // 0和负认为符号相同
+//    int sameSign(int a, int b) {
+//        if ((a > 0 && b > 0)
+//            || (a <= 0 && b <= 0))
+//            return 1;
+//        else
+//            return 0;
+//    }
 
+    int divide(int dividend, int divisor) {
+        // 特例
+        if (dividend == 0 || divisor == 0) return 0;
+        // 判断符号是否相同
+        bool flag = (dividend > 0 && divisor > 0) || (dividend <= 0 && divisor <= 0);
+        // 全部取正，不能取反，会溢出，比如INT_MIN
+//        if (dividend < 0) dividend = -dividend;
+//        if (divisor < 0) divisor = -divisor;
+        // 循环
+        int pool = flag ? dividend - divisor : dividend + divisor;
+        int ret = 0;
+        while ((dividend > 0 && pool >= 0) || (dividend < 0 && pool <= 0)) {
+            ++ret;
+            pool = flag ? pool - divisor : pool + divisor;
+        }
+        return flag ? ret : -ret;
     }
 };
 
 int main() {
-
+    Solution s;
+    int ret = s.divide(3, 3);
+    cout << ret << endl;
 }
