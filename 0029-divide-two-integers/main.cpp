28730fb7dffd4fb55b00028ac9d7c50b56bce061
silianpan <liu.pan@silianpan.cn>
2021-03-20 04:40:17 +0800

    算法28

---
+++ 0029-divide-two-integers/main.cpp
@@ -0,0 +1,22 @@
+//
+// Copyright (c) 2021 Seals Studio, All rights reserved.
+// @license: GPL
+// @author: silianpan
+// @email: liu.pan@silianpan.cn
+// @date 2021/2/25
+// @brief: 0029 两数相除
+//
+
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+
+    }
+};
+
+int main() {
+
+}
