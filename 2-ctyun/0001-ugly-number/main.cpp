5430ea0a30b0cee3d73db832de78caebcf5cd278
silianpan <1206284818@qq.com>
2023-04-06 22:22:05 +0800

    feat: 丑数

--- 2-ctyun/0001-ugly-number/main.cpp
+++ 2-ctyun/0001-ugly-number/main.cpp
@@ -1,17 +1,16 @@
 //
 // Created by liupan on 2023/4/6.
-// 题目：把只包含质因子2、3和5的数称作丑数（Ugly Number）。例如6、8都是丑数，但14不是，因为它包含质因子7。
+// 题目：把只包含质因子2、3和5的数称作丑数（Ugly
+// Number）。例如6、8都是丑数，但14不是，因为它包含质因子7。
 // 习惯上我们把1当做是第一个丑数。求按从小到大的顺序的第N个丑数。
 //
 
 class Solution {
 public:
-    int getUglyNumber(int n) {
-
-    }
+  int getUglyNumber(int n) {}
 };
 
 int main() {
-    Solution s;
-    return 0;
+  Solution s;
+  return 0;
 }
